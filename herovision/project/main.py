9a87975f7a6c4650ded36d02721fe7463d9368a5
Matthew <matthew.harrison@2itesting.com>
2021-11-06 09:53:26 +0000

    Unified project init

---
+++ herovision/project/main.py
@@ -0,0 +1,11 @@
+#makeshift entry point
+
+googleauth = input("please insert your google cloud token:")
+accepted = True
+
+#TODO AUTH
+
+if accepted:
+    print("your auth token has been accepted")
+else:
+    print("your auth token is not recognised")
---
+++ herovision/project/readme.md
@@ -0,0 +1,3 @@
+#Herovision
+###Unified project
+Run this project by opening a terminal at this location and running "python main.py".
